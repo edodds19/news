5c940efc6d8cfad9474c0f65e1f495736e349e7b
edodds19 <eric.dodds@kellogg.northwestern.edu>
2020-05-20 16:35:21 +0000

    Final Updates

--- app.rb
+++ app.rb
@@ -55,7 +55,9 @@ get "/" do
   
   @title0 = news ["articles"][0]["title"]
   @url0 = news ["articles"][0]["url"]
+  @source0 = news ["articles"][0]["source"]["name"]
   @description0 = news ["articles"][0]["description"]
+  @photo0 = news ["articles"][0]["urlToImage"]
 
   @title1 = news ["articles"][1]["title"]
   @url1 = news ["articles"][1]["url"]
